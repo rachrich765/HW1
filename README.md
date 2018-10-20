d8f7c4044de781b192a2415f8c0c1efdc4177aaf
rachrich765 <rachjr@umich.edu>
2018-10-20 14:44:05 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,25 +1,65 @@
-# Homework 1 - SI 364 F18
+# Homework 3 - SI 364 F18
 
-## DEADLINE: Sunday September 16, 2018 at 11:59 PM
+### Deadline: October 21, 2018 at 11:59 PM
+
+## To submit
+
+* Fork and clone this repository.
+* Submit the link to your fork to the Canvas assignment as specified on Canvas.
 
 ## Instructions
 
-See inside the `SI364W18_HW1.py` file for details about the four problems to complete in this assignment.
+There are a number of tasks to complete, mainly in the `SI364W18_HW3.py` file, in order to replicate **[this application](http://sample364hw3.herokuapp.com/)** locally.
 
-When you are done, there should be at least these 5 routes that work in the Flask app in `SI364W18_HW1.py`:
+Each of the things you need to do is marked and described inside the `SI364W18_HW3.py` file, with a comment including **`TODO 364`**. For some, there are also hints provided.
 
-* `/` (works in the provided code; should not change)
-* `/class` (from Problem 1)
-* `/movie/<anytitlesearch>` (from Problem 2)
-* `/question` (from Problem 3)
-* `/problem4form` (from Problem 4)
+**NOTE:** If you don't already have your text editor set to *wrap* lines, you should change it so that it does -- that will make it MUCH easier to read the instructions. You can do that in the menu *View > Toggle Soft Wrap* in Atom and similar in Sublime Text.
 
-There may be 1 or 2 more, depending upon how you decide to handle Problems 2-4, but you only need these five working correctly and following all requirements to succeed at completing this homework assignment.
+The majority of the 1000 total points for this assignment come from, approximately in order:
 
-## To complete the assignment and submit
+* The correct view functions to fill in that are provided (`index`, `see_all_users`, `see_all_tweets`)
+* Creating the form and custom validation for it
+* Defining the models
+* Adding the `longest_tweet` route properly (see instructions in the file)
+* Doing the app setup (e.g. creating a database) according to the instructions
 
-Fork this repository.
+However, that is *not* the order in which you should complete these tasks, because some can't be done without the others!
 
-To work on it, clone the repository and make edits and commits. Push them to your clone of the repository.
+*We will not grade code that does not run.* Make sure yours runs, even if it does not have every feature you want in it.
 
-Submit the link to YOUR repository clone to the assignment for **HW 1** on the SI 364 Canvas site.
+**We recommend this approximate order of tasks:**
+
+* Consider and write/draw out the things you'll need to do and how data will move through the application. Make sure you understand the problem(s) at hand, and read through all the comments describing the assignment.
+* First do all the app setup. Make sure you've created a database, etc, and fill in that URL...
+* Define the form and test it out.
+* Define your models.
+* Work on, and then test, your scaffolded view functions, including making the database queries (which you can also check with the Flask shell as shown in class!)
+* Add the `longest_tweet` view function and corresponding template.
+* Test it all again.
+
+
+## Files and structure included
+
+### Provided:
+
+* Custom errorhandler routes for 404 and 500 errors
+* Custom error templates 404.html and 500.html
+* Other templates:
+    * `base.html` (the basis from which others inherit)
+    * `index.html`
+    * `all_users.html`
+    * `all_tweets.html`
+* Necessary import statements and some parts of necessary app setup
+* Scaffolding comments inside `SI364W18_HW3.py`
+
+### You should add:
+
+* A form class definition
+* Model class definitions
+* A bunch of code, as specified in the `SI364W18_HW3.py` file
+* 1 template (see the instructions for `longest_route` at the end of the .py file)
+* Comments at the top of your file specifying anyone with whom you worked on this HW
+
+**Important:** (This HW is not exactly like any from past semesters, but note that it is unethical to use others' work from this semester or other semesters or to look at old semester's work and use it as your own.)
+
+You should *not* change any file names or edit provided templates.
---
+++ SI364W18_HW3.py
@@ -0,0 +1,261 @@
+## SI 364 - Winter 2018
+## HW 3
+
+####################
+## Import statements
+####################
+
+from flask import Flask, render_template, session, redirect, url_for, flash, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import Required, Length
+from flask_sqlalchemy import SQLAlchemy
+
+#I did not work with anyone on this project.
+############################
+# Application configurations
+############################
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string from si364'
+## TODO 364: Create a database in postgresql in the code line below, and fill in your app's database URI. It should be of the format: postgresql://localhost/YOUR_DATABASE_NAME
+
+## Your final Postgres database should be your uniqname, plus HW3, e.g. "jczettaHW3" or "maupandeHW3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:4Rocks32@localhost/rachjrHW3"
+## Provided:
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+##################
+### App setup ####
+##################
+db = SQLAlchemy(app) # For database use
+
+
+#########################
+#########################
+######### Everything above this line is important/useful setup,
+## not problem-solving.##
+#########################
+#########################
+
+#########################
+##### Set up Models #####
+#########################
+
+## TODO 364: Set up the following Model classes, as described, with the respective fields (data types).
+
+## The following relationships should exist between them:
+# Tweet:User - Many:One
+class Tweet(db.Model):
+    __tablename__ = "tweets"
+    id = db.Column(db.Integer,primary_key=True)
+    text = db.Column(db.String(280))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    def __repr__(self):
+        return "{} (ID: {})".format(self.text,self.user_id)
+# - Tweet
+## -- id (Integer, Primary Key)
+## -- text (String, up to 280 chars)
+## -- user_id (Integer, ID of user posted -- ForeignKey)
+
+## Should have a __repr__ method that returns strings of a format like:
+#### {Tweet text...} (ID: {tweet id})
+
+
+# - User
+## -- id (Integer, Primary Key)
+## -- username (String, up to 64 chars, Unique=True)
+## -- display_name (String, up to 124 chars)
+## ---- Line to indicate relationship between Tweet and User tables (the 1 user: many tweets relationship)
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    display_name = db.Column(db.String(124))
+    tweet = db.relationship('Tweet',backref='User')
+    def __repr__(self):
+        return "{} (ID: {})".format(self.username,self.id)
+## Should have a __repr__ method that returns strings of a format like:
+#### {username} | ID: {id}
+
+
+########################
+##### Set up Forms #####
+########################
+
+# TODO 364: Fill in the rest of the below Form class so that someone running this web app will be able to fill in information about tweets they wish existed to save in the database:
+
+## -- text: tweet text (Required, should not be more than 280 characters)
+## -- username: the twitter username who should post it (Required, should not be more than 64 characters)
+## -- display_name: the display name of the twitter user with that username (Required, + set up custom validation for this -- see below)
+
+# HINT: Check out index.html where the form will be rendered to decide what field names to use in the form class definition
+
+# TODO 364: Set up custom validation for this form such that:
+# - the twitter username may NOT start with an "@" symbol (the template will put that in where it should appear)
+# - the display name MUST be at least 2 words (this is a useful technique to practice, even though this is not true of everyone's actual full name!)
+
+# TODO 364: Make sure to check out the sample application linked in the readme to check if yours is like it!
+def validate_username(form, field):
+        if str(field.data)[:1] == '@':
+            raise ValidationError('username cannot start with an "@" symbol!')
+def validate_displayname(form, field):
+        if len(str(field.data).split()) < 2:
+            raise ValidationError('display name must be at least 2 words!')
+class TweetForm(FlaskForm):
+    text = StringField('Enter the text of the tweet (no more than 280 chars)', validators=[Required(), Length(max=280)])
+    username = StringField(' Enter the username of the twitter user (no "@"!)', validators=[Required(), Length(max=64), validate_username]) 
+    display_name = StringField('Enter the display name for the twitter user (must be at least 2 words):', validators=[Required(), validate_displayname])
+    submit = SubmitField('Submit')
+     
+###################################
+##### Routes & view functions #####
+###################################
+
+## Error handling routes - PROVIDED)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+#############
+## Main route
+#############
+
+## TODO 364: Fill in the index route as described.
+
+# A template index.html has been created and provided to render what this route needs to show -- YOU just need to fill in this view function so it will work.
+# Some code already exists at the end of this view function -- but there's a bunch to be filled in.
+## HINT: Check out the index.html template to make sure you're sending it the data it needs.
+## We have provided comment scaffolding. Translate those comments into code properly and you'll be all set!
+
+# NOTE: The index route should:
+# - Show the Tweet form.
+# - If you enter a tweet with identical text and username to an existing tweet, it should redirect you to the list of all the tweets and a message that you've already saved a tweet like that.
+# - If the Tweet form is entered and validates properly, the data from the form should be saved properly to the database, and the user should see the form again with a message flashed: "Tweet successfully saved!"
+# Try it out in the sample app to check against yours!
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # Initialize the form
+    tweets1 = Tweet.query.all()
+    num_tweets = len(tweets1)
+    form = TweetForm()
+    if form.validate_on_submit():
+        tweet1 = form.text.data
+        user_name = form.username.data
+        displayname = form.display_name.data 
+        user = User.query.filter_by(username=user_name).first()
+        tweet12 = Tweet.query.filter_by(text=tweet1).first()
+        if not user: 
+            user = User(username=user_name, display_name=displayname)
+            db.session.add(user) 
+            db.session.commit()
+            if not tweet12:
+                tweet12 = Tweet(text=tweet1,user_id=user.id)
+                db.session.add(tweet12)
+                db.session.commit()
+                flash("Tweet was successfully added to the database")
+                return render_template('index.html',form=form, num_tweets=num_tweets)
+            else:
+                flash("This tweet already exists in the database")
+                return see_all_tweets()
+        if user:
+            if not tweet12:
+                tweet12 = Tweet(text=tweet1,user_id=user.id)
+                db.session.add(tweet12)
+                db.session.commit()
+                flash("Tweet was successfully added to the database")
+                return render_template('index.html',form=form, num_tweets=num_tweets)
+            else:
+                flash("This tweet already exists in the database")
+                return see_all_tweets()
+
+
+    ## If there is, save it in a variable: user
+    ## Or if there is not, then create one and add it to the database
+
+    ## If there already exists a tweet in the database with this text and this user id (the id of that user variable above...) ## Then flash a message about the tweet already existing
+    ## And redirect to the list of all tweets
+
+    ## Assuming we got past that redirect,
+    ## Create a new tweet object with the text and user id
+    ## And add it to the database
+    ## Flash a message about a tweet being successfully added
+    ## Redirect to the index page
+
+    # PROVIDED: If the form did NOT validate / was not submitted
+    errors = [v for v in form.errors.values()]
+    if len(errors) > 0:
+        flash("!!!! ERRORS IN FORM SUBMISSION - " + str(errors))
+    return render_template('index.html', form=form, num_tweets=num_tweets) # TODO 364: Add more arguments to the render_template invocation to send data to index.html
+
+@app.route('/all_tweets')
+def see_all_tweets():
+    all_tweets = []
+    tweets = Tweet.query.all()
+    for tw in tweets:
+        user12 = User.query.filter_by(id=tw.user_id).first()
+        all_tweets.append((tw.text, user12.username))
+    return render_template('all_tweets.html',all_tweets=all_tweets)
+    # TODO 364: Fill in this view function so that it can successfully render the template all_tweets.html, which is provided.
+    # HINT: Careful about what type the templating in all_tweets.html is expecting! It's a list of... not lists, but...
+    # HINT #2: You'll have to make a query for the tweet and, based on that, another query for the username that goes with it...
+
+
+@app.route('/all_users')
+def see_all_users():
+    all_users = []
+    us = User.query.all()
+    for us1 in us:
+        user1 = User.query.filter_by(id=us1.id).first()
+        all_users.append((user1.username, user1.display_name))
+    return render_template('all_users.html',users=all_users)
+    # TODO 364: Fill in this view function so it can successfully render the template all_users.html, which is provided.
+
+# TODO 364
+# Create another route (no scaffolding provided) at /longest_tweet with a view function get_longest_tweet (see details below for what it should do)
+# TODO 364
+# Create a template to accompany it called longest_tweet.html that extends from base.html.
+@app.route('/longest_tweet')
+def long_non_whitespace():
+    att = []
+    longest_user_info = []
+    all_tweets_text = db.session.query(Tweet.text).all()
+    for a in all_tweets_text:
+        att.append(str(a[0]))
+    longest_tweet = str(att[0])
+    longest_len = len(longest_tweet)
+    for x in att:
+        if sum(c != ' ' for c in x) > longest_len:
+            longest_tweet = x
+        if sum(c != ' ' for c in x) == longest_len:
+            if (x.toLowerCase().charAt(0) < longest_tweet.toLowerCase().charAt(0)):
+                longest_tweet = x
+            else:
+                longest_tweet = longest_tweet
+    longest_tweet_info = db.session.query(Tweet).filter_by(text=longest_tweet)
+    for l in longest_tweet_info:
+        user_longest_tweet = User.query.filter_by(id=l.user_id).first()
+        longest_user_info.append((longest_tweet, user_longest_tweet.username, user_longest_tweet.display_name))
+    return render_template('longest_route.html', longest_user_info=longest_user_info)
+    
+# NOTE:
+# This view function should compute and render a template (as shown in the sample application) that shows the text of the tweet currently saved in the database which has the most NON-WHITESPACE characters in it, and the username AND display name of the user that it belongs to.
+# NOTE: This is different (or could be different) from the tweet with the most characters including whitespace!
+# Any ties should be broken alphabetically (alphabetically by text of the tweet). HINT: Check out the chapter in the Python reference textbook on stable sorting.
+# Check out /longest_tweet in the sample application for an example.
+
+# HINT 2: The chapters in the Python reference textbook on:
+## - Dictionary accumulation, the max value pattern
+## - Sorting
+# may be useful for this problem!
+
+
+if __name__ == '__main__':
+    db.create_all() # Will create any defined models when you run the application
+    app.run(use_reloader=True,debug=True) # The usual
